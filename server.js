ea90ada90fba7a86a46a68e6c0225dd427dc9c78
Sagnik Biswas <60578512+sbiswas2209@users.noreply.github.com>
2020-08-22 11:53:27 +0530

    Added Update

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/delete', function(req,res){
     res.sendFile('pages/delete.html', {root: __dirname});
 });
 
+app.get('/update', function(req,res){
+    res.sendFile('pages/update.html', {root: __dirname});
+});
+
 app.get('/', function(req,res){
     res.sendFile('pages/home.html', {root: __dirname});
 });
@@ -41,6 +45,7 @@ app.post('/add', (req,res) => {
     let flag  = false;
 
     for(i = 0 ; i < email.length ; i++){
+        //console.log('Verifying data');
         if(email.charAt(i) == '@'){
             flag = true;
         }
@@ -52,15 +57,17 @@ app.post('/add', (req,res) => {
             'email' : email,
             'password' : hashedPassword,
         });
+        res.send(`Added user : ${email}`);
     }
 
     else{
         console.log('Error!');
+        res.send('Error!');
     }
 
     console.log(db);
 
-    res.send(`Added user : ${email}`);
+    
 
 });
 
@@ -74,10 +81,74 @@ app.post('/verify', (req,res) => {
         console.log(db[i]['email'] + db[i]['password']);
         if((email == db[i]['email']) && (password_hash.verify(password , db[i]['password']))){
             flag = true;
+            break;
         }
     }
     if(flag == true){
         res.send('User Verified');
+    }
+    else{
+        res.send('User not found');
+    }
+});
+
+app.post('/delete', (req,res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let i = 0;
+    let n = 0;
+    let flag = false;
+    for(i = 0 ; i < db.length ; i++){
+        console.log(db[i]['email'] + db[i]['password']);
+        if((email == db[i]['email']) && (password_hash.verify(password , db[i]['password']))){
+            flag = true;
+            n = i;
+            break;
+        }
+    }
+
+    if(flag == true){
+        db.splice(n, 1);
+        console.log(db);
+        res.send('User Deleted');
+    }
+
+
+
+});
+
+
+app.post('/update', (req,res) => {
+    let email = req.body.email;
+    console.log(email);
+    let password = req.body.password;
+    console.log(req.body.password);
+    let newEmail = req.body.emailNew;
+    console.log(req.body.emaiNew);
+    let newPassword = req.body.passwordNew;
+    console.log(req.body.passwordNew);
+    let i = 0;
+    let n = 0;
+    let flag = false;
+    for(i = 0 ; i < db.length ; i++){
+        console.log(db[i]['email'] + db[i]['password']);
+        if((email == db[i]['email']) && (password_hash.verify(password , db[i]['password']))){
+            console.log('Found');
+            flag = true;
+            n = i;
+            break;
+        }
+    }
+    
+    if(flag==true){
+        let newHashPassword = password_hash.generate(newPassword);
+        db.splice(n , 1 , {
+            'email' : newEmail,
+            'password' : newHashPassword,            
+        });
+        console.log(db);
+        res.send('Updated User');
     }
     else{
         res.send('User not found');
